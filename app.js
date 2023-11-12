60b572f50e52fa60b18c0eafa436be93f729099f
Kimberly Pecco <Kim.pecco2.0@gmail.com>
2023-11-12 08:01:06 -0500

    updating the js and html

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Add a click event listener to the search button
     document.getElementById('searchBtn').addEventListener('click', function () {
+        // Get the search input value
+        var searchQuery = document.getElementById('searchInput').value;
+
+        // Sanitize the search query (you may use a more robust sanitization method)
+        var sanitizedQuery = encodeURIComponent(searchQuery);
+        
         // Make an AJAX call to superheroes.php
         var xhr = new XMLHttpRequest();
 
         xhr.onreadystatechange = function () {
             if (xhr.readyState == 4 && xhr.status == 200) {
-                // On success, show the list of superheroes in an alert
-                var superheroes = JSON.parse(xhr.responseText);
-                alert("List of Superheroes:\n" + superheroes.join(', '));
+                // On success, Display the result in the "result " div
+                document.getElementById("result").innerHTML = xhr.responseText;
             } else if (xhr.readyState == 4 && xhr.status != 200) {
                 // On error, show an alert
-                alert("Error fetching data. Status code: " + xhr.status);
+                document.getElementById('result').innerHTML = "Error fetching data. Status code " + xhr.status;
             }
         };
 
-        xhr.open('GET', 'superheroes.php', true);
+        xhr.open('GET', 'superheroes.php?query=' + sanitizedQuery, true);
         xhr.send();
     });
 });
