afa9e44b4f52bf602cb0d6d6beebdc7425c95588
Kimberly Pecco <Kim.pecco2.0@gmail.com>
2023-11-12 08:24:52 -0500

    nothing is right but commit

--- app.js
+++ app.js
@@ -6,17 +6,33 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Sanitize the search query (you may use a more robust sanitization method)
         var sanitizedQuery = encodeURIComponent(searchQuery);
-        
-        // Make an AJAX call to superheroes.php
+
+        // Make an AJAX call to superheroes.php with the search query as a parameter
         var xhr = new XMLHttpRequest();
 
         xhr.onreadystatechange = function () {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                // On success, Display the result in the "result " div
-                document.getElementById("result").innerHTML = xhr.responseText;
-            } else if (xhr.readyState == 4 && xhr.status != 200) {
-                // On error, show an alert
-                document.getElementById('result').innerHTML = "Error fetching data. Status code " + xhr.status;
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    // On success, parse the JSON response
+                    var response = JSON.parse(xhr.responseText);
+
+                    // Check if a superhero was found
+                    if (response.length > 0) {
+                        // Display the details of the first superhero in the "result" div
+                        var superhero = response[0];
+                        document.getElementById('result').innerHTML = `
+                            <h3>${superhero.alias}</h3>
+                            <h4>${superhero.name}</h4>
+                            <p>${superhero.biography}</p>
+                        `;
+                    } else {
+                        // If superhero not found, display a message
+                        document.getElementById('result').innerHTML = "Superhero not found";
+                    }
+                } else {
+                    // On error, display an error message
+                    document.getElementById('result').innerHTML = "Error fetching data. Status code: " + xhr.status;
+                }
             }
         };
 
