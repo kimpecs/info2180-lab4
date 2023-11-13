62f09b26d4cbf81d82604007172850ee5f68e347
Kimberly Pecco <Kim.pecco2.0@gmail.com>
2023-11-12 23:27:55 -0500

    trying to fix the list

--- app.js
+++ app.js
@@ -1,41 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
     const searchButton = document.getElementById('searchButton');
     const searchInput = document.getElementById('searchInput');
-    const resultDiv = document.getElementById('result');
+    const superheroesList = document.getElementById('superheroesList');
 
     searchButton.addEventListener('click', function () {
         const searchTerm = searchInput.value.trim().toLowerCase();
 
         if (searchTerm !== '') {
-            // Make AJAX request
-            const xhr = new XMLHttpRequest();
-            xhr.open('GET', `http://localhost/info2180-lab4/superheroes.php?query=${encodeURIComponent(searchTerm)}`, true);
-
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState === XMLHttpRequest.DONE) {
-                    if (xhr.status === 200) {
-                        const response = JSON.parse(xhr.responseText);
-                        displayResult(response);
-                    } else {
-                        displayResult({ error: 'An error occurred while fetching data.' });
+            fetch(`http://localhost/info2180-lab4/superheroes.php?query=${encodeURIComponent(searchTerm)}`)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('An error occurred while fetching data.');
                     }
-                }
-            };
-
-            xhr.send();
+                    return response.json();
+                })
+                .then(data => {
+                    displayResult(data);
+                })
+                .catch(error => {
+                    displayResult({ error: error.message });
+                });
         } else {
-            // If search input is empty, display the original list
-            displayResult({ superheroes:superheroes });
+            displayResult({ superheroes });
         }
     });
 
     function displayResult(data) {
-        const superheroesList = document.getElementById('superheroesList');
-        superheroesList.innerHTML = ''; 
-    
+        superheroesList.innerHTML = '';
+
         if (data.error) {
             superheroesList.innerHTML = `<p>${data.error}</p>`;
-        } else if (data.superheroes.length > 0) {
+        } else if (data.superheroes && data.superheroes.length > 0) {
             data.superheroes.forEach(superhero => {
                 const listItem = document.createElement('li');
                 listItem.textContent = superhero.alias;
@@ -45,5 +40,4 @@ document.addEventListener('DOMContentLoaded', function () {
             superheroesList.innerHTML = '<p>Superhero not found</p>';
         }
     }
-    
 });
