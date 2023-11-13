4004b192f57ad98cfaae20e043fba4a1ebc34204
Kimberly Pecco <Kim.pecco2.0@gmail.com>
2023-11-12 20:08:09 -0500

    ..

--- app.js
+++ app.js
@@ -1,42 +1,59 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Add a click event listener to the search button
-    document.getElementById('searchBtn').addEventListener('click', function () {
-        // Get the search input value
-        var searchQuery = document.getElementById('searchInput').value;
+    const searchButton = document.getElementById('searchButton');
+    const searchInput = document.getElementById('searchInput');
+    const resultDiv = document.getElementById('result');
 
-        // Sanitize the search query (you may use a more robust sanitization method)
-        var sanitizedQuery = encodeURIComponent(searchQuery);
+    searchButton.addEventListener('click', function () {
+        const searchTerm = searchInput.value.trim();
 
-        // Make an AJAX call to superheroes.php with the search query as a parameter
-        var xhr = new XMLHttpRequest();
+        if (searchTerm !== '') {
+            // Make AJAX request
+            const xhr = new XMLHttpRequest();
+            xhr.open('GET', `superheroes.php?query=${encodeURIComponent(searchTerm)}`, true);
 
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState == 4) {
-                if (xhr.status == 200) {
-                    // On success, parse the JSON response
-                    var response = JSON.parse(xhr.responseText);
-
-                    // Check if a superhero was found
-                    if (response.length > 0) {
-                        // Display the details of the first superhero in the "result" div
-                        var superhero = response[0];
-                        document.getElementById('result').innerHTML = `
-                            <h3>${superhero.alias}</h3>
-                            <h4>${superhero.name}</h4>
-                            <p>${superhero.biography}</p>
-                        `;
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === XMLHttpRequest.DONE) {
+                    if (xhr.status === 200) {
+                        const response = JSON.parse(xhr.responseText);
+                        displayResult(response);
                     } else {
-                        // If superhero not found, display a message
-                        document.getElementById('result').innerHTML = "Superhero not found";
+                        displayResult({ error: 'An error occurred while fetching data.' });
                     }
-                } else {
-                    // On error, display an error message
-                    document.getElementById('result').innerHTML = "Error fetching data. Status code: " + xhr.status;
                 }
-            }
-        };
+            };
 
-        xhr.open('GET', 'superheroes.php?query=' + sanitizedQuery, true);
-        xhr.send();
+            xhr.send();
+        } else {
+            // If search input is empty, display the original list
+            displayResult({ superheroes: originalSuperheroes });
+        }
     });
+
+    function displayResult(data) {
+        resultDiv.innerHTML = ''; // Clear previous results
+
+        if (data.error) {
+            resultDiv.innerHTML = `<p>${data.error}</p>`;
+        } else if (data.superheroes.length > 0) {
+            // Display the list or a single superhero
+            if (data.superheroes.length === 1) {
+                const superhero = data.superheroes[0];
+                resultDiv.innerHTML = `
+                    <h3>${superhero.alias}</h3>
+                    <h4>${superhero.name}</h4>
+                    <p>${superhero.biography}</p>
+                `;
+            } else {
+                const list = document.createElement('ul');
+                data.superheroes.forEach(superhero => {
+                    const listItem = document.createElement('li');
+                    listItem.textContent = superhero.alias;
+                    list.appendChild(listItem);
+                });
+                resultDiv.appendChild(list);
+            }
+        } else {
+            resultDiv.innerHTML = '<p>Superhero not found</p>';
+        }
+    }
 });
